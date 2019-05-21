1f785d82c68583cc10f87d02e13ef3b34f62c5d7
KyleEgland <kyle.egland@outlook.com>
2019-05-21 09:57:25 -0500

    Conducting testing with routes to ensure data is passing back and forth

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import jsonify
 from app.main import bp
 import requests
+import json
 
 
 # @bp.before_app_request
@@ -23,24 +24,28 @@ def index():
 @bp.route('/gettoken', methods=['POST'])
 def gettoken():
     # The caller should be our javascript apt which will pass in a dictionary
-    request_info = request.data()
+    if request.get_json():
+        content = request.get_json()
 
-    target = request_info['target']
+        return jsonify({'test_response': 'data exists',
+                        'test_data': content['target']})
 
-    headers = request_info['headers']
+    # target = request_info['target']
+    #
+    # headers = request_info['headers']
+    #
+    # auth_req = requests.post(target, headers=headers, verify=False)
+    #
+    # status = auth_req.status_code
+    #
+    # auth_req.encoding = 'utf-8'
 
-    auth_req = requests.post(target, headers=headers, verify=False)
+    # json_resp = {
+    #     'test_response': request_info,
+    # }
 
-    status = auth_req.status_code
-
-    auth_req.encoding = 'utf-8'
-
-    json_resp = {
-        'status': status,
-        'response': auth_req.text
-    }
-
-    return jsonify(json_resp)
+    else:
+        return jsonify({'test_response': 'an error happend'})
 
 
 @bp.route('/getapirequest', methods=['POST'])
