5178b0c31f7e68fbe24eea1bb6bc332cd8cbc803
KyleEgland <kyle.egland@outlook.com>
2019-05-21 09:55:54 -0500

    Removed some unused imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 import os
 from flask import Flask
-from flask import request
-from flask import current_app
+# from flask import request
+# from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
